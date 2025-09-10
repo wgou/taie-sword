62319b7e45f23aba8361836703a470d882244000
luochen <gw574813284@sina.com>
2025-09-11 00:31:55 +0700

    2222

--- access-server/rust_jni/src/entities/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod user;
-pub mod order;
-
-pub use user::Entity as User;
-pub use order::Entity as Order;
--- access-server/rust_jni/src/services/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod user_service;
-pub mod order_service;
-
-pub use user_service::UserService;
-pub use order_service::OrderService;
