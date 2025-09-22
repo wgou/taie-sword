be7ea78c27cb2e05729fec12df5e07a3d3cc1e75
luochen <gw574813284@sina.com>
2025-09-22 21:34:06 +0700

    Update CorsConfig.java

--- taie-service/renren-admin/src/main/java/io/renren/common/config/CorsConfig.java
+++ taie-service/renren-admin/src/main/java/io/renren/common/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+        
         		.allowedOriginPatterns("*")
                 .allowedOrigins("*")
                 .allowCredentials(true)
