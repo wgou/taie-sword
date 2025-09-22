68ec11848e5549ad95f01b3a2b57e687ec2d9cb6
luochen <gw574813284@sina.com>
2025-09-22 21:43:37 +0700

    1111

--- taie-admin-vue/src/constants/config.ts
+++ taie-admin-vue/src/constants/config.ts
@@ -3,7 +3,7 @@
  */
 export const themeSetting = {
   "sidebar": "dark",
-  "topHeader": "primary",
+  "topHeader": "dark",
   "themeColor": "#0BB2D4",
   "navLayout": "left",
   "contentFull": true,
