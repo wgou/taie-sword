c06d5d3895cc54824614fe6a42cbd864ee2eee47
luochen <gw574813284@sina.com>
2025-11-16 16:35:31 +0700

    11

--- taie-service/renren-admin/src/main/java/io/renren/modules/app/mapper/TransferMapper.java
+++
@@ -1,9 +0,0 @@
-package io.renren.modules.app.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import io.renren.modules.app.entity.Transfer;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TransferMapper extends BaseMapper<Transfer> {
-}
--- taie-service/renren-admin/src/main/java/io/renren/modules/app/service/TransferService.java
+++
@@ -1,7 +0,0 @@
-package io.renren.modules.app.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import io.renren.modules.app.entity.Transfer;
-
-public interface TransferService extends IService<Transfer> {
-}
