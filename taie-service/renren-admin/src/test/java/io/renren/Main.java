483ba18b73dd0cfec1828b5f30684a7352b6daa0
luochen <gw574813284@sina.com>
2025-11-16 16:17:23 +0700

    11

--- taie-service/renren-admin/src/test/java/io/renren/Main.java
+++ taie-service/renren-admin/src/test/java/io/renren/Main.java
@@ -1,10 +1,9 @@
 package io.renren;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.Base64;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
@@ -13,6 +12,29 @@ import io.renren.common.utils.HttpUtils;
 
 public class Main {
     public static void main(String[] args) {
+    	 String imagePath = "C:\\Users\\PC\\Downloads\\JdTPN00aKw.jpg"; // 图片路径
+         try {
+             String base64 = encodeImageToBase64(imagePath);
+             System.out.println(base64); // 输出 Base64 字符串
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+    	
+    }
+    
+    public static String encodeImageToBase64(String imagePath) throws IOException {
+        File file = new File(imagePath);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] bytes = new byte[(int) file.length()];
+        fileInputStream.read(bytes);
+        fileInputStream.close();
+
+        // 使用 Base64 编码
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+    
+    public void test1() {
+
     	Map<String, String> headers = Maps.newHashMap();
     	headers.put("pkg", "com.ghost.rc");
     	headers.put("device_id", "339f5558bec48058");
@@ -26,5 +48,6 @@ public class Main {
 			e.printStackTrace();
 		}
         
+    
     }
 }
