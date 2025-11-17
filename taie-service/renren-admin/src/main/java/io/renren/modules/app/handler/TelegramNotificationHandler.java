ef1c9d2f75746963ce530755d7f133d68024bbaa
luochen <gw574813284@sina.com>
2025-11-17 22:36:05 +0700

    11

--- taie-service/renren-admin/src/main/java/io/renren/modules/app/handler/TelegramNotificationHandler.java
+++ taie-service/renren-admin/src/main/java/io/renren/modules/app/handler/TelegramNotificationHandler.java
@@ -14,8 +14,8 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 
@@ -26,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@Profile("prod")
 public class TelegramNotificationHandler {
 
     @Autowired
