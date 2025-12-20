6251a942e42281887c95dc7abb6e304b763e0d92
luochen <gw574813284@sina.com>
2025-12-20 21:48:32 +0700

    11

--- taie-service/renren-admin/src/main/java/io/renren/modules/app/param/StatisticsParam.java
+++ taie-service/renren-admin/src/main/java/io/renren/modules/app/param/StatisticsParam.java
@@ -2,6 +2,9 @@ package io.renren.modules.app.param;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.renren.common.utils.DateUtils;
 import lombok.Data;
 
 @Data
@@ -10,9 +13,10 @@ public class StatisticsParam extends  PageParam{
 	
 	private Integer type;
 	
+	 @JsonFormat(pattern = DateUtils.DATE_TIME_PATTERN)
 	private Date start ;
 	
-	
+	 @JsonFormat(pattern = DateUtils.DATE_TIME_PATTERN)
 	private Date end ;
 	
 }
